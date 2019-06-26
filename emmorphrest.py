e0b2dfc9824ff19cc3827b4a99b9136d54490838
Balázs Indig <indba@digitus.itk.ppke.hu>
2019-06-26 17:37:23 +0200

    Add lazy init for faster startup

--- emmorphrest.py
+++ emmorphrest.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: utf-8, vim: expandtab:ts=4 -*-
 
-from xtsv import init_everything, pipeline_rest_api
+from xtsv import pipeline_rest_api, singleton_store_factory
 from emmorphpy.emmorphpy import EmMorphPy
 
 em_morph_stem = (EmMorphPy, (), {'task': 'stem', 'source_fields': {'form'}, 'target_fields': ['anas']})
 em_morph_dstem = (EmMorphPy, (), {'task': 'dstem', 'source_fields': {'form'}, 'target_fields': ['anas']})
 em_morph_analyze = (EmMorphPy, (), {'task': 'analyze', 'source_fields': {'form'}, 'target_fields': ['anas']})
 
-
 tools = {'stem': em_morph_stem, 'dstem': em_morph_dstem, 'analyze': em_morph_analyze}
 
-inited_tools = init_everything({k: v for k, v in tools.items()})
-
-app = pipeline_rest_api('emMorph', inited_tools, {},  False)
+app = pipeline_rest_api('emMorph', tools, {},  conll_comments=False, singleton_store=singleton_store_factory())
 
 if __name__ == '__main__':
     app.run(debug=False)
