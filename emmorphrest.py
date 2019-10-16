96f03df32ee0f9716f8425b7dda15669f8556a45
Balázs Indig <indba@digitus.itk.ppke.hu>
2019-10-16 18:27:58 +0200

    Enable REST API frontend through xtsv

--- emmorphrest.py
+++ emmorphrest.py
@@ -2,15 +2,20 @@
 # -*- coding: utf-8, vim: expandtab:ts=4 -*-
 
 from xtsv import pipeline_rest_api, singleton_store_factory
-from emmorphpy.emmorphpy import EmMorphPy
 
-em_morph_stem = (EmMorphPy, (), {'task': 'stem', 'source_fields': {'form'}, 'target_fields': ['anas']})
-em_morph_dstem = (EmMorphPy, (), {'task': 'dstem', 'source_fields': {'form'}, 'target_fields': ['anas']})
-em_morph_analyze = (EmMorphPy, (), {'task': 'analyze', 'source_fields': {'form'}, 'target_fields': ['anas']})
+em_morph_stem = ('emmorphpy.emmorphpy', 'EmMorphPy', 'stem', (),
+                 {'task': 'stem', 'source_fields': {'form'}, 'target_fields': ['anas']})
+em_morph_analyze = ('emmorphpy.emmorphpy', 'EmMorphPy', 'analyze', (),
+                    {'task': 'analyze', 'source_fields': {'form'}, 'target_fields': ['anas']})
+em_morph_dstem = ('emmorphpy.emmorphpy', 'EmMorphPy', 'dstem', (),
+                  {'task': 'dstem', 'source_fields': {'form'}, 'target_fields': ['anas']})
 
-tools = {'stem': em_morph_stem, 'dstem': em_morph_dstem, 'analyze': em_morph_analyze}
-
-app = pipeline_rest_api('emMorph', tools, {},  conll_comments=False, singleton_store=singleton_store_factory())
+tools = [(em_morph_stem, ('stem',)),
+         (em_morph_analyze, ('analyze',)),
+         (em_morph_dstem, ('dstem',)),
+         ]
+app = pipeline_rest_api('emMorph', tools, {},  conll_comments=False, singleton_store=singleton_store_factory(),
+                        form_title='emMorph demo', form_type='radio', doc_link='https://github.com/ppke-nlpg/emmorphpy')
 
 if __name__ == '__main__':
     app.run(debug=False)
